c72869d0dc25d4bf3358e918f1ed6be24337c3d2
github username <github email address>
2018-05-10 21:33:53 -0400

    Finish style and finish todo list

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,3 +17,7 @@ $("input[type='text']").keypress(function(event) {
     $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
   }
 });
+
+$(".fa-plus").click(function() {
+  $("input[type='text']").fadeToggle();
+});
