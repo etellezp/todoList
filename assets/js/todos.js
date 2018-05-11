fe375a1b14f7291c19e02b942b0348f744176710
github username <github email address>
2018-05-10 21:27:54 -0400

    Work on style and added font awesome for icons

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -14,6 +14,6 @@ $("input[type='text']").keypress(function(event) {
     var todoText = $(this).val();
     $(this).val("");
 
-    $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
   }
 });
